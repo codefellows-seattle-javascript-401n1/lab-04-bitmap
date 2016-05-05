b808ddd17fce0aae818c42e6e297628258993fc3
Matthew Smith <mattsmith203@hotmail.com>
2016-05-04 21:26:39 -0700

    added parser.js

--- bitmap-ittybitty/test/head-test.js
+++ bitmap-ittybitty/test/head-test.js
@@ -8,11 +8,10 @@ describe('Testing the bitmap head reader module', function(){
   describe('testing head function', function(){
     it('should return "424d462b00000000000036040000"', function(done){
       fs.readFile(__dirname + '/../img/bitmap1.bmp', function(err, data){
-          if (err){throw err
-          };
-          var headspc = data.toString('hex', 0, 14);
-          expect(headspc).to.equal("424d462b00000000000036040000");
-          done();
+        if (err) throw err;
+        var headspc = data.toString('hex', 0, 14);
+        expect(headspc).to.equal('424d462b00000000000036040000');
+        done();
       });
     });
   });
