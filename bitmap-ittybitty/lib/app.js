4cdf0df893f0d43193354078b440935fd0de0596
Matthew Smith <mattsmith203@hotmail.com>
2016-05-05 20:45:37 -0700

    expanded cont function and added toBuffer function

--- bitmap-ittybitty/lib/app.js
+++ bitmap-ittybitty/lib/app.js
@@ -4,22 +4,76 @@
 const fs = require('fs');
 const bitmap = fs.readFileSync(__dirname +'/../img/bitmap1.bmp');
 
-function AllThatData(){
-  this.headField = bitmap.toString('ascii', 0, 2);
-  this.size = bitmap.readUInt32LE(2);
-  this.pixelArrayStart = bitmap.readUInt32LE(10);
-  this.colorPaletteSize = bitmap.readUInt32LE(46);
-  this.onecolor = bitmap.toString('hex',58,62);
-  this.onepixel = bitmap.toString('ascii', 1078, 1079);
-  this.paletteStart = (this.pixelArrayStart - this.colorPaletteSize*4);
+const AllThatData = module.exports = function (buffer){
+  // file header
+  this.headField = buffer.toString('utf8', 0, 2);
+  this.size = buffer.readUInt32LE(2);
+  this.reservedOne = buffer.readUInt16LE(6);
+  this.reservedtwo = buffer.readUInt16LE(8);
+  this.pixelArrayStart = buffer.readUInt32LE(10);
 
-  // console.dir(this);
-}
+  // dib header
+  this.headerSize = buffer.readUInt32LE(14);
+  this.width = buffer.readUInt32LE(18);
+  this.height = buffer.readUInt32LE(22);
+  this.colorPanes = buffer.readUInt16LE(26);
+  this.bitsPerPixel = buffer.readUInt16LE(28);
+  this.compressionMethod = buffer.readUInt32LE(30);
+  this.imgSize = buffer.readUInt32LE(34);
+  this.horizontal = buffer.readUInt32LE(38);
+  this.vertical = buffer.readUInt32LE(42);
+  this.numColor = buffer.readUInt32LE(46);
+  this.numImpColor = buffer.readUInt32LE(48);
 
-var bitmapData = new AllThatData;
-console.log(bitmapData);
+
+  // this.colorPaletteSize = bitmap.readUInt32LE(46);
+  // this.onecolor = bitmap.toString('hex',58,62);
+  // this.onepixel = bitmap.toString('utf8', 1078, 1079);
+  this.paletteStart = (this.pixelArrayStart - this.numColor*4);
+  // this.paletteLength;
+
+};
+
+
+AllThatData.prototype.toBuffer = function(){
+  const result = new Buffer(this.size);
+  result.write(this.headField);
+  result.writeInt32LE(this.size, 2);
+  result.writeInt16LE(this.reservedOne, 6);
+  result.writeInt16LE(this.reservedtwo, 8);
+  result.writeInt32LE(this.pixelArrayStart, 10);
+  result.writeInt32LE(this.headerSize, 14);
+  result.writeInt32LE(this.width, 18);
+  result.writeInt32LE(this.height, 22);
+  result.writeInt16LE(this.colorPanes, 26);
+  result.writeInt16LE(this.bitsPerPixel, 28);
+  result.writeInt32LE(this.compressionMethod, 30);
+  result.writeInt32LE(this.imgSize, 34);
+  result.writeInt32LE(this.horizontal, 38);
+  result.writeInt32LE(this.vertical, 42);
+  result.writeInt32LE(this.numColor, 46);
+  result.writeInt32LE(this.numImpColor, 48);
+
+  console.log('my buffer', result);
+
+};
+
+const coolImage = new AllThatData(bitmap);
+coolImage.toBuffer();
+
+// var bitmapData = new AllThatData(bitmap);
+// console.log(bitmapData);
+//
+// AllThatData.prototype.backToBuf = function (data) {
+//   var rebuf = new Buffer(data.buffer);
+//   console.log(rebuf);
+// };
+//
+// AllThatData.backToBuf();
+// var rebuf = new Buffer(bitmap);
+// console.log(bitmap);
 
 
 
-var start = bitmapData.paletteStart;
-var finish = bitmapData.pixelArrayStart;
+// var start = bitmapData.paletteStart;
+// var finish = bitmapData.pixelArrayStart;
