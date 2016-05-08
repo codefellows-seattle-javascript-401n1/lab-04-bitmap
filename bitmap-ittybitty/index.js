a439041f9115f3f7c19246b26e72ec05e822d4a9
Tuesday-Last <sgnortz@gmail>
2016-05-08 09:01:09 -0700

    consolidation the modules into index

--- bitmap-ittybitty/index.js
+++ bitmap-ittybitty/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 const fs = require('fs');
-const handler = require('handler');
-const parser = require('parser');
+const handler = require(__dirname + '/lib/handler');
+const app = require(__dirname + '/lib/app');
+const transformer = require('./lib/transformer');
+const bitmap = fs.readFileSync(__dirname +'/img/bitmap1.bmp');
+
+var bufferOne = handler.reader(bitmap);
+var parsedBitmap = new app.AllThatData(bufferOne);
+var newBitmap = transformer.getBitmapData(parsedBitmap);
+newBitmap.toBuffer();
+handler.writer(newBitmap.result);
