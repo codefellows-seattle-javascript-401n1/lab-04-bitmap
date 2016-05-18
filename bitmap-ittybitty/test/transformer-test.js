4c59486d9d6dab9cac36c86e54f7c93ab12bdb86
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-05-18 16:41:11 -0700

    add lint and test gulp tasks

--- bitmap-ittybitty/test/transformer-test.js
+++ bitmap-ittybitty/test/transformer-test.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const fs = require('fs');
-const app = require('./../lib/transformer');
+const transformer = require('./../lib/transformer');
 const expect = require('chai').expect;
 const app = require('./../lib/app');
-const img = require('/../img/bitmap1.bmp')
-const imgData = new app(img);
+// const img = require('/../img/bitmap1.bmp') //This cannot be required. Not a module.
+// const imgData = new app(img);
 
-describe('Testing the transformer module', function(){
-  describe('testing getBitmapData function', function(){
-    it('it should create a with ', function(){
-      transformer.getBitmapData(imgData){
-
-      });
-    });
-  });
-});
+// describe('Testing the transformer module', function(){
+//   describe('testing getBitmapData function', function(){
+//     it('it should create a with ', function(){
+//       transformer.getBitmapData(imgData){
+//
+//       });
+//     });
+//   });
+// });
