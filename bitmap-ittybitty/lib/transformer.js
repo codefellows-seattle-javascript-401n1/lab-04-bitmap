8bbd891341733ee8da18a86cc04dbeadc91748a5
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-05-18 17:00:06 -0700

    set TODO's, delete extraneous files

--- bitmap-ittybitty/lib/transformer.js
+++ bitmap-ittybitty/lib/transformer.js
@@ -1,8 +1,5 @@
 'use strict';
 
-
-const fs = require('fs');
-
 function transform(palette) {
   for (let i = 0; i < palette.length ; i++) {
     palette[i] = palette[i] + 2;
--- bitmap-ittybitty/test/transformer-test.js
+++ bitmap-ittybitty/test/transformer-test.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const transformer = require('./../lib/transformer');
 const expect = require('chai').expect;
 const app = require('./../lib/app');
+
+//TODO: We need to test the transformer functions 'transformAdd2' and 'randomColor'
+
 // const img = require('/../img/bitmap1.bmp') //This cannot be required. Not a module.
 // const imgData = new app(img);
 
