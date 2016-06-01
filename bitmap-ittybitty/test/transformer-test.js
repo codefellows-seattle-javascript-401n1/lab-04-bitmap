a2d8e62d23e9a0340fc09e2c4d309c4458c59610
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-06-01 08:45:50 -0700

    add transformer module testing

--- bitmap-ittybitty/test/transformer-test.js
+++ bitmap-ittybitty/test/transformer-test.js
@@ -1,21 +1,24 @@
 'use strict';
 
-const fs = require('fs');
 const transformer = require('./../lib/transformer');
 const expect = require('chai').expect;
-const app = require('./../lib/app');
 
-//TODO: We need to test the transformer functions 'transformAdd2' and 'randomColor'
+describe('Testing transformer module', function(){
+  describe('Testing transform function', function(){
+    it('should alter a buffer by adding 2 to each byte', function(){
+      var buff = new Buffer(8);
+      var plusTwoBuff = transformer.transformAdd2(buff);
+      expect(buff).to.equal(plusTwoBuff);
+    });
+  });
+});
 
-// const img = require('/../img/bitmap1.bmp') //This cannot be required. Not a module.
-// const imgData = new app(img);
-
-// describe('Testing the transformer module', function(){
-//   describe('testing getBitmapData function', function(){
-//     it('it should create a with ', function(){
-//       transformer.getBitmapData(imgData){
-//
-//       });
-//     });
-//   });
-// });
+describe('Testing transformer module', function(){
+  describe('Testing randomColor function', function(){
+    it('should alter a buffer using randomization', function(){
+      var buff = new Buffer(8);
+      var randomizedBuff = transformer.randomColor(buff);
+      expect(buff).to.equal(randomizedBuff);
+    });
+  });
+});
